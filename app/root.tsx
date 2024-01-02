f7044699259b4ce2d43611dcd0eb266350d68871
Chung Nguyen <zrmedia9@gmail.com>
2024-01-02 16:18:30 +0700

    update code

--- app/root.tsx
+++ app/root.tsx
@@ -1,6 +1,7 @@
 import { cssBundleHref } from '@remix-run/css-bundle';
 import type { LinksFunction } from '@remix-run/node';
 import {
+  Link,
   Links,
   LiveReload,
   Meta,
@@ -35,6 +36,36 @@ export default function App() {
           <MainNavigation />
         </header>
         <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
+
+export const ErrorBoundary = ({ error }: any) => {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1"
+        />
+        <Meta />
+        <Links />
+        <title>An error occurred!</title>
+      </head>
+      <body>
+        <header>
+          <MainNavigation />
+        </header>
+        <main>
+          <h1>An error occurred!</h1>
+          <p>{error.message}</p>
+          <p>Back to <Link to=".">safety</Link></p>
+        </main>
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
--- app/routes/notes.tsx
+++ app/routes/notes.tsx
@@ -1,10 +1,12 @@
-import { ActionFunctionArgs, json, redirect } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
+import { ActionFunctionArgs, redirect } from '@remix-run/node';
+import { useLoaderData } from '@remix-run/react';
 
-import NewNote, { links as newNoteLinks } from "~/components/NewNote";
-import NoteList, { links as noteListLinks } from "~/components/NoteList";
-import { getStoredNotes, storeNotes } from "~/data/notes";
-import Note from "~/models/Note";
+import NewNote, { links as newNoteLinks } from '~/components/NewNote';
+import NoteList, {
+  links as noteListLinks,
+} from '~/components/NoteList';
+import { getStoredNotes, storeNotes } from '~/data/notes';
+import Note from '~/models/Note';
 
 export default function NotesPage() {
   const notes = useLoaderData<Array<Note>>();
@@ -19,20 +21,24 @@ export default function NotesPage() {
 export const loader = async () => {
   const notes = await getStoredNotes();
   return notes;
-}
+};
 
-export const action = async ({
-  request,
-}: ActionFunctionArgs) => {
+export const action = async ({ request }: ActionFunctionArgs) => {
   const formData = await request.formData();
   const noteData = Object.fromEntries(formData);
+
+  if (noteData.title.trim().length < 5) {
+    return { message: 'Invalid title - must be at least 5 characters long.' };
+  }
+
   const existingNotes = await getStoredNotes();
   noteData.id = new Date().toISOString();
   const updateNotes = existingNotes.concat(noteData);
   await storeNotes(updateNotes);
+  // await new Promise<void>((resolve) => setTimeout(() => resolve(), 2000));
   return redirect('/notes');
-}
+};
 
 export const links = () => {
   return [...newNoteLinks(), ...noteListLinks()];
-}
+};
